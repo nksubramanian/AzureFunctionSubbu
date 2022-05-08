e3ab0b70922b12300baa5a0421f6594aaf05dbad
subramanian.kumarappan <subramanian.nk@live.com>
2022-05-08 10:03:48 +0530

    getting configuration values

--- DataProviderService/Sperry.MxA.DataProvider.Functions/HttpTriggers/DataProviderRequestTrigger.cs
+++ DataProviderService/Sperry.MxA.DataProvider.Functions/HttpTriggers/DataProviderRequestTrigger.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 
 using Newtonsoft.Json;
 using System.IO;
+using static Sperry.MxA.DataProvider.Functions.Program;
 
 namespace Sperry.MxA.DataProvider.Functions.HttpTriggers
 {
@@ -24,6 +25,9 @@ namespace Sperry.MxA.DataProvider.Functions.HttpTriggers
         [Function("v1/DataProvider/ProcessRequest")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req, FunctionContext executionContext)
         {
+            var t = ConfigurationManager.datamodel;
+            var g = t.key1;
+
             _logger.LogInformation(@"DataProviderRequest trigger functions is called subramanian");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
--- DataProviderService/Sperry.MxA.DataProvider.Functions/Program.cs
+++ DataProviderService/Sperry.MxA.DataProvider.Functions/Program.cs
@@ -10,18 +10,33 @@ namespace Sperry.MxA.DataProvider.Functions
     {
         public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
                                 .SetBasePath(Directory.GetCurrentDirectory())
-                                .AddJsonFile($"appsettings.Development.json", optional: true)
+                                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
                                 .AddJsonFile("hosting.json", optional: true)
                                 .AddEnvironmentVariables()
                                 .Build();
         public static void Main()
         {
-
+            ConfigurationManager.datamodel = Configuration.GetSection("Chart").Get<Datamodel>();
             var host = new HostBuilder()
                         .ConfigureFunctionsWorkerDefaults()
                         .Build();
             host.Run();
         }
+
+        public class Datamodel
+        {
+            public string key1 { get; set; }
+            public string key2 { get; set; }
+            public string key3 { get; set; }
+        }
+
+        public static class ConfigurationManager
+        {
+            public static Datamodel datamodel { get; set; }
+
+        }
+
+
     }
 
 
