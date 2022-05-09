703892d1b1224fb96478fe33f2dad491d1d159da
subramanian.kumarappan <subramanian.nk@live.com>
2022-05-09 11:48:11 +0530

    getting the operation Id

--- IndependentFunctions/IndependentFunctions/Program.cs
+++ IndependentFunctions/IndependentFunctions/Program.cs
@@ -1,5 +1,10 @@
+using IndependentFunctions;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.IO;
 
@@ -18,11 +23,45 @@ namespace DependentFunctionAzure
         {
 
             var host = new HostBuilder()
-                        .ConfigureFunctionsWorkerDefaults()
+                       
+                        .ConfigureFunctionsWorkerDefaults(configure =>
+                        {
+                            // other middleware also configure following way.
+                            // It will execute in same order it is configured over here.
+                            configure.UseMiddleware<SimpleMiddleware>();
+                        })
+                        .ConfigureServices(services =>
+                        {
+                            services.AddSingleton<ITelemetryInitializer, CloudRoleNameTelemetryInitializer>();
+                        })
                         .Build();
             host.Run();
         }
     }
 
 
+    public class CloudRoleNameTelemetryInitializer : ITelemetryInitializer
+    {
+        private readonly ILogger _logger;
+        public CloudRoleNameTelemetryInitializer(ILoggerFactory logger)
+        {
+            _logger = logger.CreateLogger("CloudRoleNameTelemetryInitializer");
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            string c = telemetry.Context.Operation.Id;
+            _logger.LogInformation("the operation Id is " + c);
+
+            // set custom role name here
+            if (c != null)
+            {
+                //telemetry.Context.Operation.Id = "12345-" + c.Substring(3);
+            }
+
+        }
+    }
+
+
 }
+
